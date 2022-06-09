31de1401d64ba8208962252da1454b6464d64fa2
BR <reeseman04@gmail.com>
2022-06-09 08:56:37 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 SCRIPT_VERSION="135"
-SCRIPT_URL='https://raw.githubusercontent.com/amidaware/tacticalrmm/master/update.sh'
-LATEST_SETTINGS_URL='https://raw.githubusercontent.com/amidaware/tacticalrmm/master/api/tacticalrmm/tacticalrmm/settings.py'
+SCRIPT_URL='https://raw.githubusercontent.com/scs-ben/tacticalrmm/master/update.sh'
+LATEST_SETTINGS_URL='https://raw.githubusercontent.com/scs-ben/tacticalrmm/master/api/tacticalrmm/tacticalrmm/settings.py'
 YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 THIS_SCRIPT=$(readlink -f "$0")
 
-SCRIPTS_DIR="/opt/trmm-community-scripts"
-PYTHON_VER="3.10.4"
+SCRIPTS_DIR='/opt/trmm-community-scripts'
+PYTHON_VER='3.10.4'
+SETTINGS_FILE='/rmm/api/tacticalrmm/tacticalrmm/settings.py'
 
 TMP_FILE=$(mktemp -p "" "rmmupdate_XXXXXXXXXX")
 curl -s -L "${SCRIPT_URL}" > ${TMP_FILE}
@@ -46,7 +47,6 @@ fi
 
 TMP_SETTINGS=$(mktemp -p "" "rmmsettings_XXXXXXXXXX")
 curl -s -L "${LATEST_SETTINGS_URL}" > ${TMP_SETTINGS}
-SETTINGS_FILE="/rmm/api/tacticalrmm/tacticalrmm/settings.py"
 
 LATEST_TRMM_VER=$(grep "^TRMM_VERSION" "$TMP_SETTINGS" | awk -F'[= "]' '{print $5}')
 CURRENT_TRMM_VER=$(grep "^TRMM_VERSION" "$SETTINGS_FILE" | awk -F'[= "]' '{print $5}')
@@ -59,11 +59,9 @@ fi
 
 LATEST_MESH_VER=$(grep "^MESH_VER" "$TMP_SETTINGS" | awk -F'[= "]' '{print $5}')
 LATEST_PIP_VER=$(grep "^PIP_VER" "$TMP_SETTINGS" | awk -F'[= "]' '{print $5}')
-LATEST_NPM_VER=$(grep "^NPM_VER" "$TMP_SETTINGS" | awk -F'[= "]' '{print $5}')
 NATS_SERVER_VER=$(grep "^NATS_SERVER_VER" "$TMP_SETTINGS" | awk -F'[= "]' '{print $5}')
 
 CURRENT_PIP_VER=$(grep "^PIP_VER" "$SETTINGS_FILE" | awk -F'[= "]' '{print $5}')
-CURRENT_NPM_VER=$(grep "^NPM_VER" "$SETTINGS_FILE" | awk -F'[= "]' '{print $5}')
 
 cls() {
   printf "\033c"
@@ -113,10 +111,6 @@ do
 printf >&2 "${GREEN}Stopping ${i} service...${NC}\n"
 sudo systemctl stop ${i}
 done
-
-printf >&2 "${GREEN}Restarting postgresql database${NC}\n"
-sudo systemctl restart postgresql
-sleep 5
 
 rm -f /rmm/api/tacticalrmm/app.ini
 
@@ -232,7 +226,7 @@ git pull
 if [[ ! -d ${SCRIPTS_DIR} ]]; then
   sudo mkdir -p ${SCRIPTS_DIR}
   sudo chown ${USER}:${USER} ${SCRIPTS_DIR}
-  git clone https://github.com/amidaware/community-scripts.git ${SCRIPTS_DIR}/
+  git clone https://github.com/scs-ben/community-scripts.git ${SCRIPTS_DIR}/
   cd ${SCRIPTS_DIR}
   git config user.email "admin@example.com"
   git config user.name "Bob"
@@ -301,6 +295,8 @@ python manage.py load_chocos
 python manage.py create_installer_user
 python manage.py create_natsapi_conf
 python manage.py post_update_tasks
+API=$(python manage.py get_config api)
+WEB_VERSION=$(python manage.py get_config webversion)
 deactivate
 
 printf >&2 "${GREEN}Turning off redis aof${NC}\n"
@@ -308,18 +304,22 @@ sudo redis-cli config set appendonly no
 sudo redis-cli config rewrite
 sudo rm -f /var/lib/redis/appendonly.aof
 
-rm -rf /rmm/web/dist
-rm -rf /rmm/web/.quasar
-cd /rmm/web
-if [[ "${CURRENT_NPM_VER}" != "${LATEST_NPM_VER}" ]] || [[ "$force" = true ]]; then
-  rm -rf /rmm/web/node_modules
+
+if [ -d /rmm/web ]; then
+  rm -rf /rmm/web
 fi
 
-npm install
-npm run build
+if [ ! -d /var/www/rmm ]; then
+  sudo mkdir -p /var/www/rmm
+fi
+
+webtar="trmm-web-v${WEB_VERSION}.tar.gz"
+wget -q https://github.com/scs-ben/tacticalrmm-web/releases/download/v${WEB_VERSION}/${webtar} -O /tmp/${webtar}
 sudo rm -rf /var/www/rmm/dist
-sudo cp -pr /rmm/web/dist /var/www/rmm/
+sudo tar -xzf /tmp/${webtar} -C /var/www/rmm
+echo "window._env_ = {PROD_URL: \"https://${API}\"}" | sudo tee /var/www/rmm/dist/env-config.js > /dev/null
 sudo chown www-data:www-data -R /var/www/rmm/dist
+rm -f /tmp/${webtar}
 
 for i in nats nats-api rmm daphne celery celerybeat nginx
 do
